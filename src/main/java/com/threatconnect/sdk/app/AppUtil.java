d5ef71fde1dd6757c6c8b9b59109f7e3a65790c5
Greg Marut <gmarut@threatconnect.com>
2015-12-08 13:01:02 -0500

    added the getters for the proxy information

--- src/main/java/com/threatconnect/sdk/app/AppUtil.java
+++ src/main/java/com/threatconnect/sdk/app/AppUtil.java
@@ -1,6 +1,5 @@
 package com.threatconnect.sdk.app;
 
-
 import java.io.IOException;
 import java.util.Map;
 import java.util.logging.FileHandler;
@@ -12,111 +11,126 @@ import java.util.logging.SimpleFormatter;
 /**
  * Created by dtineo on 5/15/15.
  */
-public class AppUtil
-{
-    private Type type = Type.System;
-    private Map<String,String> map = null;
+public class AppUtil {
+	private Type type = Type.System;
+	private Map<String, String> map = null;
 
-    private static enum Type {
-        System, Map
-    }
+	private static final String TC_PROXY_HOST = "tc_proxy_host";
+	private static final String TC_PROXY_PORT = "tc_proxy_port";
+	private static final String TC_PROXY_USERNAME = "tc_proxy_username";
+	private static final String TC_PROXY_PASSWORD = "tc_proxy_password";
 
-    public static AppUtil getMapInstance(Map<String,String> map)
-    {
-        AppUtil appUtil = new AppUtil();
-        appUtil.type = Type.Map;
-        appUtil.map = map;
+	private static enum Type {
+		System, Map
+	}
 
-        return appUtil;
-    }
+	public static AppUtil getMapInstance(Map<String, String> map) {
+		AppUtil appUtil = new AppUtil();
+		appUtil.type = Type.Map;
+		appUtil.map = map;
 
-    public static AppUtil getSystemInstance()
-    {
-        AppUtil appUtil = new AppUtil();
-        appUtil.type = Type.System;
+		return appUtil;
+	}
 
-        return appUtil;
-    }
+	public static AppUtil getSystemInstance() {
+		AppUtil appUtil = new AppUtil();
+		appUtil.type = Type.System;
 
-    protected AppUtil() {}
+		return appUtil;
+	}
 
-    protected String p(String propName)
-    {
-        return this.type==Type.Map ? map.get(propName) : System.getProperty(propName);
-    }
+	protected AppUtil() {
+	}
 
-    public String getTcLogPath() {
-        return p("tc_log_path");
-    }
+	protected String p(String propName) {
+		return this.type == Type.Map ? map.get(propName) : System.getProperty(propName);
+	}
 
-    public String getTcTempPath() {
-        return p("tc_temp_path");
-    }
+	public String getTcLogPath() {
+		return p("tc_log_path");
+	}
 
-    public String getTcOutPath() {
-        return p("tc_out_path");
-    }
+	public String getTcTempPath() {
+		return p("tc_temp_path");
+	}
 
-    public String getTcApiPath() {
-        return p("tc_api_path");
-    }
+	public String getTcOutPath() {
+		return p("tc_out_path");
+	}
 
-    public String getTcApiAccessID() {
-        return p("api_access_id");
-    }
+	public String getTcApiPath() {
+		return p("tc_api_path");
+	}
 
-    public String getTcApiUserSecretKey() {
-        return p("api_secret_key");
-    }
+	public String getTcProxyHost() {
+		return p(TC_PROXY_HOST);
+	}
+	
+	public String getTcProxyPort() {
+		return p(TC_PROXY_PORT);
+	}
+	
+	public String getTcProxyUsername() {
+		return p(TC_PROXY_USERNAME);
+	}
+	
+	public String getTcProxyPassword() {
+		return p(TC_PROXY_PASSWORD);
+	}
+	
+	public String getTcApiAccessID() {
+		return p("api_access_id");
+	}
 
-   public String getApiDefaultOrg() {
-        return p("api_default_org");
-    }
+	public String getTcApiUserSecretKey() {
+		return p("api_secret_key");
+	}
 
-    public Integer getApiMaxResults() {
-        return getApiMaxResults(350);
-    }
+	public String getApiDefaultOrg() {
+		return p("api_default_org");
+	}
+	
+	public Integer getApiMaxResults() {
+		return getApiMaxResults(350);
+	}
 
-    public Integer getApiMaxResults(int defaultMax) {
-        String p = p("api_max_results");
-        return p == null ? defaultMax : Integer.valueOf(defaultMax);
-    }
+	public Integer getApiMaxResults(int defaultMax) {
+		String p = p("api_max_results");
+		return p == null ? defaultMax : Integer.valueOf(defaultMax);
+	}
 
-    public String getOwner() {
-        return p("owner");
-    }
+	public String getOwner() {
+		return p("owner");
+	}
 
-    public Level getTcLogLevel() {
-        return getTcLogLevel("INFO");
-    }
+	public Level getTcLogLevel() {
+		return getTcLogLevel("INFO");
+	}
 
-    public Level getTcLogLevel(String defaultLevel) {
-        String level = p("tc_log_level");
+	public Level getTcLogLevel(String defaultLevel) {
+		String level = p("tc_log_level");
 
-        return level == null ? Level.parse(defaultLevel) : Level.parse(level.toUpperCase());
-    }
+		return level == null ? Level.parse(defaultLevel) : Level.parse(level.toUpperCase());
+	}
 
-    public static void configureLogger(String logFilename, Level level) throws IOException
-    {
+	public static void configureLogger(String logFilename, Level level) throws IOException {
 
-        System.err.println("configureLogger....");
-        // suppress the logging output to the console
-        /*
-        Logger rootLogger = Logger.getLogger("");
-        Handler[] handlers = rootLogger.getHandlers();
-        if (handlers[0] instanceof ConsoleHandler) {
-            rootLogger.removeHandler(handlers[0]);
-        }
-        */
+		System.err.println("configureLogger....");
+		// suppress the logging output to the console
+		/*
+		 * Logger rootLogger = Logger.getLogger(""); Handler[] handlers =
+		 * rootLogger.getHandlers(); if (handlers[0] instanceof ConsoleHandler)
+		 * { rootLogger.removeHandler(handlers[0]); }
+		 */
 
-        Logger logger = Logger.getLogger("");
-        logger.setLevel(level);
-        FileHandler fileHandler = new FileHandler(logFilename);
+		Logger logger = Logger.getLogger("");
+		logger.setLevel(level);
+		FileHandler fileHandler = new FileHandler(logFilename);
 
-        Formatter formatterTxt = new SimpleFormatter();
-        fileHandler.setFormatter(formatterTxt);
-        logger.addHandler(fileHandler);
+		Formatter formatterTxt = new SimpleFormatter();
+		fileHandler.setFormatter(formatterTxt);
+		logger.addHandler(fileHandler);
 
-    }
+	}
 
 }
