02022f07b178e4dc1e37f3c782b65d776d147a9e
Greg Marut <gmarut@threatconnect.com>
2016-02-09 09:02:25 -0500

    updated unit test

--- threatconnect-parser/src/test/java/com/threatconnect/sdk/app/util/UrlUtilTest.java
+++ threatconnect-parser/src/test/java/com/threatconnect/sdk/app/util/UrlUtilTest.java
@@ -23,4 +23,20 @@ public class UrlUtilTest
 		Assert.assertEquals("http://www.example.com/one/two",
 			UrlUtil.toAbsoluteURL("one/two", "http://www.example.com"));
 	}
+	
+	@Test
+	public void isAbsoluteURLTest()
+	{
+		Assert.assertTrue(UrlUtil.isAbsoluteURL("http://www.example.com/one/two"));
+		Assert.assertTrue(UrlUtil.isAbsoluteURL("http://example.com/one/two"));
+		Assert.assertTrue(UrlUtil.isAbsoluteURL("http://www.example.com"));
+		Assert.assertTrue(UrlUtil.isAbsoluteURL("http://www.example.com/"));
+		Assert.assertTrue(UrlUtil.isAbsoluteURL("http://example.com"));
+		
+		Assert.assertFalse(UrlUtil.isAbsoluteURL("www.example.com/one/two"));
+		Assert.assertFalse(UrlUtil.isAbsoluteURL("example.com/one/two"));
+		Assert.assertFalse(UrlUtil.isAbsoluteURL("www.example.com/"));
+		Assert.assertFalse(UrlUtil.isAbsoluteURL("www.example.com"));
+		Assert.assertFalse(UrlUtil.isAbsoluteURL("example.com"));
+	}
 }
