e38c71ed6025bf13dd19b5802fb28388898a9eaa
Greg Marut <gmarut@threatconnect.com>
2016-04-18 15:00:22 -0400

    document was missing from the reader

--- threatconnect-sdk-core/src/main/java/com/threatconnect/sdk/app/LoggerUtil.java
+++ threatconnect-sdk-core/src/main/java/com/threatconnect/sdk/app/LoggerUtil.java
@@ -5,10 +5,11 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.appender.FileAppender;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.apache.logging.log4j.core.layout.PatternLayout;
-
 
 public class LoggerUtil
 {
@@ -19,49 +20,55 @@ public class LoggerUtil
 	 * the log file to write the output
 	 * @param appConfig
 	 * the app configuration
-	 * @throws IOException On error
+	 * @throws IOException
+	 * On error
 	 */
 	public static void reconfigureGlobalLogger(final File logFile, final AppConfig appConfig) throws IOException
 	{
-		// retrieve the root logger
-		Logger logger = (Logger) LogManager.getRootLogger();
-		
-		reconfigureLogger(logger, logFile, appConfig);
+		reconfigureLogger(LogManager.ROOT_LOGGER_NAME, logFile, appConfig);
 	}
 	
 	/**
 	 * Reconfigures the global logger setting up the log file and the log level
 	 * 
-	 * @param logger
+	 * @param loggerPath
 	 * the logger to reconfigure
 	 * @param logFile
 	 * the log file to write the output
 	 * @param appConfig
 	 * the app configuration
-	 * @throws IOException On error
+	 * @throws IOException
+	 * On error
 	 */
-	public static void reconfigureLogger(final Logger logger, final File logFile, final AppConfig appConfig)
+	public static void reconfigureLogger(final String loggerPath, final File logFile, final AppConfig appConfig)
 		throws IOException
 	{
-		// set the log level for this logger
-		logger.setLevel(appConfig.getTcLogLevel());
-		//TODO replace the following with log4j2
-		//logger.removeAllAppenders();
+		// retrieve the logger context
+		LoggerContext loggerContext = (LoggerContext) LogManager.getContext();
+		Configuration configuration = loggerContext.getConfiguration();
+		
+		// retrieve the root logger config
+		LoggerConfig loggerConfig = configuration.getLoggerConfig(loggerPath);
+		loggerConfig.setLevel(appConfig.getTcLogLevel());
 		
 		// Define log pattern layout
-		PatternLayout layout= PatternLayout.createLayout("%-5p %d{HH:mm:ss} %c - %m%n", null, null, null, Charset.defaultCharset(),false,false,null,null);
-        
-		//PatternLayout layout = new PatternLayout("%-5p %d{HH:mm:ss} %c - %m%n");
-		
-		// Add the appender to root logger 
-		logger.addAppender(FileAppender.createAppender(logFile.getAbsolutePath(), "true", "false", "fileAppender", "true", "true", "true", "8192", layout, null, "false", null, null));
+		PatternLayout layout = PatternLayout.createLayout("%-5p %d{HH:mm:ss} %c - %m%n", null, null, null,
+			Charset.defaultCharset(), false, false, null, null);
+			
+		FileAppender fileAppender =
+			FileAppender.createAppender(logFile.getAbsolutePath(), "true", "false", "fileAppender",
+				"true", "true", "true", "8192", layout, null, "false", null, null);
+				
+		loggerConfig.addAppender(fileAppender, appConfig.getTcLogLevel(), null);
 	}
 	
 	/**
 	 * Writes a message to the standard out
 	 * 
-	 * @param msg Message to write to log
-	 * @param fmtArgs Formatter string
+	 * @param msg
+	 * Message to write to log
+	 * @param fmtArgs
+	 * Formatter string
 	 */
 	public static void logOut(final String msg, final Object... fmtArgs)
 	{
@@ -71,8 +78,10 @@ public class LoggerUtil
 	/**
 	 * Writes a message to the standard error
 	 * 
-	 * @param msg Message to write to logs
-	 * @param fmtArgs Formatter string
+	 * @param msg
+	 * Message to write to logs
+	 * @param fmtArgs
+	 * Formatter string
 	 */
 	public static void logErr(final String msg, final Object... fmtArgs)
 	{
@@ -82,9 +91,12 @@ public class LoggerUtil
 	/**
 	 * Writes a message to the standard error
 	 *
-	 * @param msg Message to write to logs
-	 * @param fmtArgs Formatter string
-	 * @param e Exception
+	 * @param msg
+	 * Message to write to logs
+	 * @param fmtArgs
+	 * Formatter string
+	 * @param e
+	 * Exception
 	 */
 	public static void logErr(final Exception e, final String msg, final Object... fmtArgs)
 	{
--- threatconnect-sdk-core/src/main/java/com/threatconnect/sdk/client/reader/ReaderAdapterFactory.java
+++ threatconnect-sdk-core/src/main/java/com/threatconnect/sdk/client/reader/ReaderAdapterFactory.java
@@ -37,190 +37,244 @@ import com.threatconnect.sdk.server.response.entity.UrlListResponse;
 import com.threatconnect.sdk.server.response.entity.UrlResponse;
 
 /**
- *
  * @author dtineo
  */
-public class ReaderAdapterFactory {
-
-    public static AbstractGroupReaderAdapter<Adversary> createAdversaryGroupReader(Connection conn) {
-        return new AbstractGroupReaderAdapter<Adversary>(conn, AdversaryResponse.class, Adversary.class, AdversaryListResponse.class) {
-            @Override
-            public String getUrlType() {
-                return "adversaries";
-            }
-        };
-    }
-
-    public static AbstractGroupReaderAdapter<Email> createEmailGroupReader(Connection conn) {
-        return new AbstractGroupReaderAdapter<Email>(conn, EmailResponse.class, Email.class, EmailListResponse.class) {
-            @Override
-            public String getUrlType() {
-                return "emails";
-            }
-        };
-    }
-
-    public static AbstractGroupReaderAdapter<Incident> createIncidentGroupReader(Connection conn) {
-        return new AbstractGroupReaderAdapter<Incident>(conn, IncidentResponse.class, Incident.class, IncidentListResponse.class) {
-            @Override
-            public String getUrlType() {
-                return "incidents";
-            }
-        };
-    }
-
-    public static AbstractGroupReaderAdapter<Signature> createSignatureGroupReader(Connection conn) {
-        return new AbstractGroupReaderAdapter<Signature>(conn, SignatureResponse.class, Signature.class, SignatureListResponse.class) {
-            @Override
-            public String getUrlType() {
-                return "signatures";
-            }
-        };
-    }
-
-    public static AbstractGroupReaderAdapter<Threat> createThreatGroupReader(Connection conn) {
-        return new AbstractGroupReaderAdapter<Threat>(conn, ThreatResponse.class, Threat.class, ThreatListResponse.class) {
-            @Override
-            public String getUrlType() {
-                return "threats";
-            }
-        };
-    }
-
-    public static AbstractIndicatorReaderAdapter<Address> createAddressIndicatorReader(Connection conn) {
-        return new AbstractIndicatorReaderAdapter<Address>(conn, AddressResponse.class, Address.class, AddressListResponse.class) {
-            @Override
-            public String getUrlType() {
-                return "addresses";
-            }
-        };
-    }
-
-    public static AbstractIndicatorReaderAdapter<EmailAddress> createEmailAddressIndicatorReader(Connection conn) {
-        return new AbstractIndicatorReaderAdapter<EmailAddress>(conn, EmailAddressResponse.class, EmailAddress.class, EmailAddressListResponse.class) {
-            @Override
-            public String getUrlType() {
-                return "emailAddresses";
-            }
-        };
-    }
-
-    public static FileIndicatorReaderAdapter createFileIndicatorReader(Connection conn) {
-        return new FileIndicatorReaderAdapter(conn);
-    }
-
-    public static AbstractIndicatorReaderAdapter<Host> createHostIndicatorReader(Connection conn) {
-        return new AbstractIndicatorReaderAdapter<Host>(conn, HostResponse.class, Host.class, HostListResponse.class) {
-            @Override
-            public String getUrlType() {
-                return "hosts";
-            }
-        };
-    }
-
-    public static AbstractIndicatorReaderAdapter<Url> createUrlIndicatorReader(Connection conn) {
-        return new AbstractIndicatorReaderAdapter<Url>(conn, UrlResponse.class, Url.class, UrlListResponse.class) {
-            @Override
-            public String getUrlType() {
-                return "urls";
-            }
-        };
-    }
-
-    public static OwnerReaderAdapter createOwnerReader(Connection conn) {
-        return new OwnerReaderAdapter(conn) {
-        };
-    }
-
-    public static SecurityLabelReaderAdapter createSecurityLabelReader(Connection conn) {
-        return new SecurityLabelReaderAdapter(conn);
-    }
-
-    public static TagReaderAdapter createTagReader(Connection conn) {
-        return new TagReaderAdapter(conn);
-    }
-
-    public static VictimReaderAdapter createVictimReader(Connection conn) {
-        return new VictimReaderAdapter(conn);
-    }
-
-    public static AbstractGroupReaderAdapter createGroupReader(String type, Connection conn) {
-        return createGroupReader(Group.Type.valueOf(type), conn);
-    }
-
-    public static AbstractGroupReaderAdapter createGroupReader(Group.Type type, Connection conn) {
-
-        switch (type) {
-            case Adversary:
-                return createAdversaryGroupReader(conn);
-            case Email:
-                return createEmailGroupReader(conn);
-            case Incident:
-                return createIncidentGroupReader(conn);
-            case Signature:
-                return createSignatureGroupReader(conn);
-            case Threat:
-                return createThreatGroupReader(conn);
-            case Task:
-                return createTaskReader(conn);
-            default:
-                return null;
-        }
-
-    }
-
-    public static AbstractIndicatorReaderAdapter createIndicatorReader(String type, Connection conn) {
-        if (Character.isLowerCase(type.charAt(0))) {
-            String indType = Character.toLowerCase(type.charAt(0)) + type.substring(1);
-            return createIndicatorReader(Indicator.Type.valueOf(indType), conn);
-        } else {
-            return createIndicatorReader(Indicator.Type.valueOf(type), conn);
-        }
-
-    }
-
-    public static AbstractIndicatorReaderAdapter createIndicatorReader(Indicator.Type type, Connection conn) {
-
-        if ( type == null )
-        {
-            return null;
-        }
-
-        switch (type) {
-            case Address:
-                return createAddressIndicatorReader(conn);
-            case EmailAddress:
-                return createEmailAddressIndicatorReader(conn);
-            case File:
-                return createFileIndicatorReader(conn);
-            case Host:
-                return createHostIndicatorReader(conn);
-            case Url:
-                return createUrlIndicatorReader(conn);
-            default:
-                return null;
-        }
-    }
-
-    /**
-     * Creates an instance of {@link DocumentReaderAdapter}.
-     *
-     * @param conn connection defining api urls
-     * @return and instance of DocumentReaderAdapter
-     */
-    public static DocumentReaderAdapter createDocumentReader(Connection conn) {
-        return new DocumentReaderAdapter(conn);
-    }
-
-    public static BatchReaderAdapter<Indicator> createIndicatorBatchReader(Connection conn) {
-        return new BatchReaderAdapter<>(conn);
-    }
-
-    public static ExchangeReaderAdapter createExchangeReader(Connection conn) {
-        return new ExchangeReaderAdapter(conn);
-    }
-
-    public static TaskReaderAdapter createTaskReader(Connection conn) {
-        return new TaskReaderAdapter(conn);
-    }
+public class ReaderAdapterFactory
+{
+	
+	public static AbstractGroupReaderAdapter<Adversary> createAdversaryGroupReader(Connection conn)
+	{
+		return new AbstractGroupReaderAdapter<Adversary>(conn, AdversaryResponse.class, Adversary.class,
+			AdversaryListResponse.class)
+		{
+			@Override
+			public String getUrlType()
+			{
+				return "adversaries";
+			}
+		};
+	}
+	
+	public static AbstractGroupReaderAdapter<Email> createEmailGroupReader(Connection conn)
+	{
+		return new AbstractGroupReaderAdapter<Email>(conn, EmailResponse.class, Email.class, EmailListResponse.class)
+		{
+			@Override
+			public String getUrlType()
+			{
+				return "emails";
+			}
+		};
+	}
+	
+	public static AbstractGroupReaderAdapter<Incident> createIncidentGroupReader(Connection conn)
+	{
+		return new AbstractGroupReaderAdapter<Incident>(conn, IncidentResponse.class, Incident.class,
+			IncidentListResponse.class)
+		{
+			@Override
+			public String getUrlType()
+			{
+				return "incidents";
+			}
+		};
+	}
+	
+	public static AbstractGroupReaderAdapter<Signature> createSignatureGroupReader(Connection conn)
+	{
+		return new AbstractGroupReaderAdapter<Signature>(conn, SignatureResponse.class, Signature.class,
+			SignatureListResponse.class)
+		{
+			@Override
+			public String getUrlType()
+			{
+				return "signatures";
+			}
+		};
+	}
+	
+	public static AbstractGroupReaderAdapter<Threat> createThreatGroupReader(Connection conn)
+	{
+		return new AbstractGroupReaderAdapter<Threat>(conn, ThreatResponse.class, Threat.class,
+			ThreatListResponse.class)
+		{
+			@Override
+			public String getUrlType()
+			{
+				return "threats";
+			}
+		};
+	}
+	
+	public static AbstractIndicatorReaderAdapter<Address> createAddressIndicatorReader(Connection conn)
+	{
+		return new AbstractIndicatorReaderAdapter<Address>(conn, AddressResponse.class, Address.class,
+			AddressListResponse.class)
+		{
+			@Override
+			public String getUrlType()
+			{
+				return "addresses";
+			}
+		};
+	}
+	
+	public static AbstractIndicatorReaderAdapter<EmailAddress> createEmailAddressIndicatorReader(Connection conn)
+	{
+		return new AbstractIndicatorReaderAdapter<EmailAddress>(conn, EmailAddressResponse.class, EmailAddress.class,
+			EmailAddressListResponse.class)
+		{
+			@Override
+			public String getUrlType()
+			{
+				return "emailAddresses";
+			}
+		};
+	}
+	
+	public static FileIndicatorReaderAdapter createFileIndicatorReader(Connection conn)
+	{
+		return new FileIndicatorReaderAdapter(conn);
+	}
+	
+	public static AbstractIndicatorReaderAdapter<Host> createHostIndicatorReader(Connection conn)
+	{
+		return new AbstractIndicatorReaderAdapter<Host>(conn, HostResponse.class, Host.class, HostListResponse.class)
+		{
+			@Override
+			public String getUrlType()
+			{
+				return "hosts";
+			}
+		};
+	}
+	
+	public static AbstractIndicatorReaderAdapter<Url> createUrlIndicatorReader(Connection conn)
+	{
+		return new AbstractIndicatorReaderAdapter<Url>(conn, UrlResponse.class, Url.class, UrlListResponse.class)
+		{
+			@Override
+			public String getUrlType()
+			{
+				return "urls";
+			}
+		};
+	}
+	
+	public static OwnerReaderAdapter createOwnerReader(Connection conn)
+	{
+		return new OwnerReaderAdapter(conn)
+		{
+		};
+	}
+	
+	public static SecurityLabelReaderAdapter createSecurityLabelReader(Connection conn)
+	{
+		return new SecurityLabelReaderAdapter(conn);
+	}
+	
+	public static TagReaderAdapter createTagReader(Connection conn)
+	{
+		return new TagReaderAdapter(conn);
+	}
+	
+	public static VictimReaderAdapter createVictimReader(Connection conn)
+	{
+		return new VictimReaderAdapter(conn);
+	}
+	
+	public static AbstractGroupReaderAdapter createGroupReader(String type, Connection conn)
+	{
+		return createGroupReader(Group.Type.valueOf(type), conn);
+	}
+	
+	public static AbstractGroupReaderAdapter createGroupReader(Group.Type type, Connection conn)
+	{
+		switch (type)
+		{
+			case Adversary:
+				return createAdversaryGroupReader(conn);
+			case Document:
+				return createDocumentReader(conn);
+			case Email:
+				return createEmailGroupReader(conn);
+			case Incident:
+				return createIncidentGroupReader(conn);
+			case Signature:
+				return createSignatureGroupReader(conn);
+			case Threat:
+				return createThreatGroupReader(conn);
+			case Task:
+				return createTaskReader(conn);
+			default:
+				throw new IllegalArgumentException(type + "is not a vaild GroupType");
+		}
+		
+	}
+	
+	public static AbstractIndicatorReaderAdapter createIndicatorReader(String type, Connection conn)
+	{
+		if (Character.isLowerCase(type.charAt(0)))
+		{
+			String indType = Character.toLowerCase(type.charAt(0)) + type.substring(1);
+			return createIndicatorReader(Indicator.Type.valueOf(indType), conn);
+		}
+		else
+		{
+			return createIndicatorReader(Indicator.Type.valueOf(type), conn);
+		}
+		
+	}
+	
+	public static AbstractIndicatorReaderAdapter createIndicatorReader(Indicator.Type type, Connection conn)
+	{
+		
+		if (type == null)
+		{
+			return null;
+		}
+		
+		switch (type)
+		{
+			case Address:
+				return createAddressIndicatorReader(conn);
+			case EmailAddress:
+				return createEmailAddressIndicatorReader(conn);
+			case File:
+				return createFileIndicatorReader(conn);
+			case Host:
+				return createHostIndicatorReader(conn);
+			case Url:
+				return createUrlIndicatorReader(conn);
+			default:
+				return null;
+		}
+	}
+	
+	/**
+	 * Creates an instance of {@link DocumentReaderAdapter}.
+	 *
+	 * @param conn
+	 * connection defining api urls
+	 * @return and instance of DocumentReaderAdapter
+	 */
+	public static DocumentReaderAdapter createDocumentReader(Connection conn)
+	{
+		return new DocumentReaderAdapter(conn);
+	}
+	
+	public static BatchReaderAdapter<Indicator> createIndicatorBatchReader(Connection conn)
+	{
+		return new BatchReaderAdapter<>(conn);
+	}
+	
+	public static ExchangeReaderAdapter createExchangeReader(Connection conn)
+	{
+		return new ExchangeReaderAdapter(conn);
+	}
+	
+	public static TaskReaderAdapter createTaskReader(Connection conn)
+	{
+		return new TaskReaderAdapter(conn);
+	}
 }
