a2a69ea2927184ac481f4d5411a6cdd9b434f110
Greg Marut <gmarut@threatconnect.com>
2016-10-05 10:26:49 -0400

    updated tc entity

--- threatconnect-sdk-addons/playbooks/src/main/java/com/threatconnect/sdk/playbooks/content/entity/TCEntity.java
+++ threatconnect-sdk-addons/playbooks/src/main/java/com/threatconnect/sdk/playbooks/content/entity/TCEntity.java
@@ -2,37 +2,59 @@ package com.threatconnect.sdk.playbooks.content.entity;
 
 public class TCEntity
 {
+	private Integer id;
 	private String type;
 	private String value;
-
+	private String ownerName;
+	
 	public TCEntity()
 	{
 		this(null, null);
 	}
-
+	
 	public TCEntity(Object type, Object value)
 	{
 		this.type = type == null ? null : type.toString();
 		this.value = value == null ? null : value.toString();
 	}
-
+	
+	public Integer getId()
+	{
+		return id;
+	}
+	
+	public void setId(final Integer id)
+	{
+		this.id = id;
+	}
+	
 	public String getType()
 	{
 		return type;
 	}
-
-	public void setType(String type)
+	
+	public void setType(final String type)
 	{
 		this.type = type;
 	}
-
+	
 	public String getValue()
 	{
 		return value;
 	}
-
-	public void setValue(String value)
+	
+	public void setValue(final String value)
 	{
 		this.value = value;
+	}
+	
+	public String getOwnerName()
+	{
+		return ownerName;
+	}
+	
+	public void setOwnerName(final String ownerName)
+	{
+		this.ownerName = ownerName;
 	}
 }
