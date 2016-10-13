7e5cfb301549c937052a743bd1a3f56e06231c41
Greg Marut <gmarut@threatconnect.com>
2016-10-13 16:17:26 -0400

    updated the tcentity object

--- threatconnect-sdk-addons/playbooks/src/main/java/com/threatconnect/sdk/playbooks/content/entity/TCEntity.java
+++ threatconnect-sdk-addons/playbooks/src/main/java/com/threatconnect/sdk/playbooks/content/entity/TCEntity.java
@@ -1,11 +1,21 @@
 package com.threatconnect.sdk.playbooks.content.entity;
 
+import java.util.Date;
+
 public class TCEntity
 {
 	private Integer id;
 	private String type;
 	private String value;
 	private String ownerName;
+	private Integer confidence;
+	private Short rating;
+	private Integer threatAssessConfidence;
+	private Short threatAssessRating;
+	private Date dateAdded;
+	private Date dateLastModified;
+	
+	private String webLink;
 	
 	public TCEntity()
 	{
@@ -20,7 +30,7 @@ public class TCEntity
 	
 	public TCEntity(Object type, Object value, String owner)
 	{
-		this(type,value);
+		this(type, value);
 		ownerName = owner;
 	}
 	
@@ -62,5 +72,75 @@ public class TCEntity
 	public void setOwnerName(final String ownerName)
 	{
 		this.ownerName = ownerName;
+	}
+	
+	public Integer getConfidence()
+	{
+		return confidence;
+	}
+	
+	public void setConfidence(final Integer confidence)
+	{
+		this.confidence = confidence;
+	}
+	
+	public Short getRating()
+	{
+		return rating;
+	}
+	
+	public void setRating(final Short rating)
+	{
+		this.rating = rating;
+	}
+	
+	public Integer getThreatAssessConfidence()
+	{
+		return threatAssessConfidence;
+	}
+	
+	public void setThreatAssessConfidence(final Integer threatAssessConfidence)
+	{
+		this.threatAssessConfidence = threatAssessConfidence;
+	}
+	
+	public Short getThreatAssessRating()
+	{
+		return threatAssessRating;
+	}
+	
+	public void setThreatAssessRating(final Short threatAssessRating)
+	{
+		this.threatAssessRating = threatAssessRating;
+	}
+	
+	public Date getDateAdded()
+	{
+		return dateAdded;
+	}
+	
+	public void setDateAdded(final Date dateAdded)
+	{
+		this.dateAdded = dateAdded;
+	}
+	
+	public Date getDateLastModified()
+	{
+		return dateLastModified;
+	}
+	
+	public void setDateLastModified(final Date dateLastModified)
+	{
+		this.dateLastModified = dateLastModified;
+	}
+	
+	public String getWebLink()
+	{
+		return webLink;
+	}
+	
+	public void setWebLink(final String webLink)
+	{
+		this.webLink = webLink;
 	}
 }
