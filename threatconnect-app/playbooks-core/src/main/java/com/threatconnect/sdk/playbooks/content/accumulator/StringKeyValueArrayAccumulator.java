1b1ae6e39ac5b3eb646f6a8c363ec74722400efd
Greg Marut <gmarut@threatconnect.com>
2016-11-03 15:28:36 -0400

    format

--- threatconnect-app/playbooks-core/src/main/java/com/threatconnect/sdk/playbooks/content/accumulator/StringKeyValueArrayAccumulator.java
+++ threatconnect-app/playbooks-core/src/main/java/com/threatconnect/sdk/playbooks/content/accumulator/StringKeyValueArrayAccumulator.java
@@ -5,6 +5,7 @@ import com.threatconnect.sdk.playbooks.content.converter.StringKeyValueListConve
 import com.threatconnect.sdk.playbooks.content.entity.StringKeyValue;
 import com.threatconnect.sdk.playbooks.db.DBService;
 import com.threatconnect.sdk.playbooks.util.StringKeyValueUtil;
+
 import java.util.List;
 
 /**
@@ -13,29 +14,29 @@ import java.util.List;
 public class StringKeyValueArrayAccumulator extends ContentAccumulator<List<StringKeyValue>>
 {
 	private final StringAccumulator stringAccumulator;
-
+	
 	public StringKeyValueArrayAccumulator(final DBService dbService)
 	{
 		super(dbService, PlaybookVariableType.KeyValueArray, new StringKeyValueListConverter());
 		this.stringAccumulator = new StringAccumulator(dbService);
 	}
-
+	
 	@Override
 	public List<StringKeyValue> readContent(final String key) throws ContentException
 	{
 		//read the key value object
 		List<StringKeyValue> results = super.readContent(key);
-
-                if (results!=null)
-                {
-                    //for each of the results
-                    for (StringKeyValue result : results)
-                    {
-                            //resolve any embedded variables in this StringKeyValue
-                            StringKeyValueUtil.resolveEmbeddedVariables(result, stringAccumulator);
-                    }
-                }
-
+		
+		if (results != null)
+		{
+			//for each of the results
+			for (StringKeyValue result : results)
+			{
+				//resolve any embedded variables in this StringKeyValue
+				StringKeyValueUtil.resolveEmbeddedVariables(result, stringAccumulator);
+			}
+		}
+		
 		return results;
 	}
 }
