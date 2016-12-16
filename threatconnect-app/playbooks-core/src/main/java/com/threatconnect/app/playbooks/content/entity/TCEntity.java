d9b29dbc2c68ed4d1f7f69d33d976796856bde92
gshepherd <gshepherd@gshepherd-wl>
2016-12-16 10:59:45 -0500

    AD-459 Added toString to TCEntity

--- threatconnect-app/playbooks-core/src/main/java/com/threatconnect/app/playbooks/content/entity/TCEntity.java
+++ threatconnect-app/playbooks-core/src/main/java/com/threatconnect/app/playbooks/content/entity/TCEntity.java
@@ -1,5 +1,7 @@
 package com.threatconnect.app.playbooks.content.entity;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import java.util.Date;
 
 public class TCEntity
@@ -14,26 +16,26 @@ public class TCEntity
 	private Short threatAssessRating;
 	private Date dateAdded;
 	private Date dateLastModified;
-	
+
 	private String webLink;
-	
+
 	public TCEntity()
 	{
-		
+
 	}
-	
+
 	public TCEntity(Object type, Object value)
 	{
 		this.type = type == null ? null : type.toString();
 		this.value = value == null ? null : value.toString();
 	}
-	
+
 	public TCEntity(Object type, Object value, String owner)
 	{
 		this(type, value);
 		ownerName = owner;
 	}
-	
+
 	public TCEntity(Integer id, String type, String value, String ownerName)
 	{
 		this.id = id;
@@ -41,114 +43,124 @@ public class TCEntity
 		this.value = value;
 		this.ownerName = ownerName;
 	}
-	
+
 	public Integer getId()
 	{
 		return id;
 	}
-	
+
 	public void setId(final Integer id)
 	{
 		this.id = id;
 	}
-	
+
 	public String getType()
 	{
 		return type;
 	}
-	
+
 	public void setType(final String type)
 	{
 		this.type = type;
 	}
-	
+
 	public String getValue()
 	{
 		return value;
 	}
-	
+
 	public void setValue(final String value)
 	{
 		this.value = value;
 	}
-	
+
 	public String getOwnerName()
 	{
 		return ownerName;
 	}
-	
+
 	public void setOwnerName(final String ownerName)
 	{
 		this.ownerName = ownerName;
 	}
-	
+
 	public Integer getConfidence()
 	{
 		return confidence;
 	}
-	
+
 	public void setConfidence(final Integer confidence)
 	{
 		this.confidence = confidence;
 	}
-	
+
 	public Short getRating()
 	{
 		return rating;
 	}
-	
+
 	public void setRating(final Short rating)
 	{
 		this.rating = rating;
 	}
-	
+
 	public Integer getThreatAssessConfidence()
 	{
 		return threatAssessConfidence;
 	}
-	
+
 	public void setThreatAssessConfidence(final Integer threatAssessConfidence)
 	{
 		this.threatAssessConfidence = threatAssessConfidence;
 	}
-	
+
 	public Short getThreatAssessRating()
 	{
 		return threatAssessRating;
 	}
-	
+
 	public void setThreatAssessRating(final Short threatAssessRating)
 	{
 		this.threatAssessRating = threatAssessRating;
 	}
-	
+
 	public Date getDateAdded()
 	{
 		return dateAdded;
 	}
-	
+
 	public void setDateAdded(final Date dateAdded)
 	{
 		this.dateAdded = dateAdded;
 	}
-	
+
 	public Date getDateLastModified()
 	{
 		return dateLastModified;
 	}
-	
+
 	public void setDateLastModified(final Date dateLastModified)
 	{
 		this.dateLastModified = dateLastModified;
 	}
-	
+
 	public String getWebLink()
 	{
 		return webLink;
 	}
-	
+
 	public void setWebLink(final String webLink)
 	{
 		this.webLink = webLink;
 	}
+
+    @Override
+    public String toString()
+    {
+        //just output in json format
+        Gson gson = new Gson();
+        java.lang.reflect.Type type = new TypeToken<TCEntity>(){}.getType();
+        String json = gson.toJson(this, type);
+        return json;
+    }
 }
