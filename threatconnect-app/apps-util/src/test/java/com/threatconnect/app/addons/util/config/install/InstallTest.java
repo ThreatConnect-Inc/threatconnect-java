6ce2016b2ef2b0aea1ee8f8fd0d8bfeb6a2362d7
Greg Marut <gmarut@threatconnect.com>
2016-12-27 11:39:21 -0500

    added some unit tests

--- threatconnect-app/apps-util/src/test/java/com/threatconnect/app/addons/util/config/install/InstallTest.java
+++ threatconnect-app/apps-util/src/test/java/com/threatconnect/app/addons/util/config/install/InstallTest.java
@@ -1,6 +1,7 @@
 package com.threatconnect.app.addons.util.config.install;
 
 import com.google.gson.Gson;
+import com.threatconnect.app.addons.util.config.install.type.PlaybookVariableType;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -30,6 +31,35 @@ public class InstallTest
 			
 			Assert.assertEquals(2, install.getParams().size());
 			Assert.assertEquals(1, install.getFeeds().size());
+		}
+	}
+	
+	@Test
+	public void loadPlaybookInstallJson() throws IOException
+	{
+		File file = new File("src/test/resources/playbook.install.json");
+		
+		try (InputStream is = new FileInputStream(file))
+		{
+			Install install = new Gson().fromJson(new InputStreamReader(is), Install.class);
+			
+			Assert.assertEquals("Sample Playbook", install.getDisplayName());
+			Assert.assertEquals("1.0.0", install.getProgramVersion());
+			Assert.assertEquals("com.threatconnect.apps.playbooks.SamplePlaybookMain", install.getProgramMain());
+			
+			Assert.assertEquals(2, install.getParams().size());
+			
+			Assert.assertEquals(3, install.getPlaybook().getOutputVariables().size());
+			Assert.assertEquals("pb.output1", install.getPlaybook().getOutputVariables().get(0).getName());
+			Assert.assertEquals("pb.output2", install.getPlaybook().getOutputVariables().get(1).getName());
+			Assert.assertEquals("pb.output3", install.getPlaybook().getOutputVariables().get(2).getName());
+			
+			Assert
+				.assertEquals(PlaybookVariableType.String, install.getPlaybook().getOutputVariables().get(0).getType());
+			Assert
+				.assertEquals(PlaybookVariableType.String, install.getPlaybook().getOutputVariables().get(1).getType());
+			Assert.assertEquals(PlaybookVariableType.KeyValueArray,
+				install.getPlaybook().getOutputVariables().get(2).getType());
 		}
 	}
 }
