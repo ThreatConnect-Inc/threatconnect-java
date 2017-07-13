84c3c7bc320cde5b44bfab8f530adff76ba7bf11
Greg Marut <gmarut@threatconnect.com>
2017-07-13 08:23:28 -0400

    upload documents in batch save

--- threatconnect-sdk-addons/threatconnect-parser/src/main/java/com/threatconnect/sdk/parser/service/save/BatchApiSaveService.java
+++ threatconnect-sdk-addons/threatconnect-parser/src/main/java/com/threatconnect/sdk/parser/service/save/BatchApiSaveService.java
@@ -2,21 +2,21 @@ package com.threatconnect.sdk.parser.service.save;
 
 import com.threatconnect.sdk.config.Configuration;
 import com.threatconnect.sdk.conn.Connection;
-import com.threatconnect.sdk.model.File;
+import com.threatconnect.sdk.model.Document;
 import com.threatconnect.sdk.model.Group;
 import com.threatconnect.sdk.model.Indicator;
 import com.threatconnect.sdk.model.Item;
 import com.threatconnect.sdk.model.ItemType;
-import com.threatconnect.sdk.model.util.IndicatorUtil;
+import com.threatconnect.sdk.model.util.GroupUtil;
 import com.threatconnect.sdk.model.util.ItemUtil;
 import com.threatconnect.sdk.parser.service.writer.BatchWriter;
+import com.threatconnect.sdk.parser.service.writer.DocumentWriter;
 import com.threatconnect.sdk.server.entity.BatchConfig.AttributeWriteType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -74,12 +74,20 @@ public class BatchApiSaveService implements SaveService
 			// save the indicators
 			SaveResults batchSaveResults = batchWriter.save(ownerName, attributeWriteType);
 			
-			//TODO: the batch api does not handle saving file occurrences so we have to revert to the ApiSaveService to do so
-			//split all of the items based on groups and indicators
 			Set<Group> groups = new HashSet<Group>();
 			Set<Indicator> indicators = new HashSet<Indicator>();
 			ItemUtil.separateGroupsAndIndicators(items, groups, indicators);
-			saveFileOccurrences(indicators, batchSaveResults);
+			
+			//upload the documents
+			Set<Document> documents = GroupUtil.extractIndicatorSet(groups, Document.class);
+			
+			//for each of the documents
+			for (Document document : documents)
+			{
+				//upload the document if there is one
+				DocumentWriter documentWriter = new DocumentWriter(connection, document);
+				documentWriter.saveGroup(ownerName);
+			}
 			
 			return batchSaveResults;
 		}
@@ -89,36 +97,6 @@ public class BatchApiSaveService implements SaveService
 			SaveResults saveResults = new SaveResults();
 			saveResults.addFailedItems(ItemType.INDICATOR, items.size());
 			return saveResults;
-		}
-	}
-	
-	/**
-	 * TODO: the batch api does not handle saving file occurrences so we have to revert to the ApiSaveService to do so
-	 *
-	 * @return
-	 */
-	private void saveFileOccurrences(final Collection<Indicator> indicators, final SaveResults saveResults)
-	{
-		//extract all of the file objects from the set of indicators
-		Set<File> files = IndicatorUtil.extractIndicatorSet(indicators, File.class);
-		ApiSaveService apiSaveService = new ApiSaveService(configuration, ownerName);
-		
-		//for each of the files
-		for (File file : files)
-		{
-			//check to see if this file has occurrences
-			if (!file.getFileOccurrences().isEmpty())
-			{
-				try
-				{
-					saveResults.addFailedItems(apiSaveService.saveItems(Collections.singletonList(file)));
-				}
-				catch (IOException e)
-				{
-					logger.warn(e.getMessage(), e);
-					saveResults.addFailedItems(file);
-				}
-			}
 		}
 	}
 	
