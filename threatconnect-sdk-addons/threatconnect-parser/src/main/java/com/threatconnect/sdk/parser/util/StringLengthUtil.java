e8ff1e2de948a5cc318bd0dda1329afda407895c
Greg Marut <gmarut@threatconnect.com>
2017-09-01 10:43:33 -0400

    updated string length util

--- threatconnect-sdk-addons/threatconnect-parser/src/main/java/com/threatconnect/sdk/parser/util/StringLengthUtil.java
+++ threatconnect-sdk-addons/threatconnect-parser/src/main/java/com/threatconnect/sdk/parser/util/StringLengthUtil.java
@@ -2,6 +2,7 @@ package com.threatconnect.sdk.parser.util;
 
 public class StringLengthUtil
 {
+	public static final String ELLIPSIS = "...";
 	public static final int ML_GROUP_NAME = 100;
 	
 	public static String trimGroupName(final String groupName)
@@ -11,12 +12,24 @@ public class StringLengthUtil
 	
 	public static String trimString(final String string, final int maxLength)
 	{
+		return trimString(string, maxLength, false);
+	}
+	
+	public static String trimString(final String string, final int maxLength, final boolean ellipsis)
+	{
 		//make sure the string is not null
 		if (null != string)
 		{
 			if (string.trim().length() > maxLength)
 			{
-				return string.trim().substring(0, maxLength);
+				if (ellipsis)
+				{
+					return string.trim().substring(0, maxLength - ELLIPSIS.length()) + ELLIPSIS;
+				}
+				else
+				{
+					return string.trim().substring(0, maxLength);
+				}
 			}
 			else
 			{
