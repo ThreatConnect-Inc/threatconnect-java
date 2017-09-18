2119ace4fec643e6ed12daaf95bdb9d89fa6a254
Chris Blades <cblades@threatconnect.com>
2017-09-18 10:11:27 -0400

    Implement equals() and hashCode()

--- threatconnect-app/playbooks-core/src/main/java/com/threatconnect/app/playbooks/content/entity/KeyValue.java
+++ threatconnect-app/playbooks-core/src/main/java/com/threatconnect/app/playbooks/content/entity/KeyValue.java
@@ -4,105 +4,106 @@ import com.threatconnect.app.addons.util.config.install.StandardPlaybookType;
 import com.threatconnect.sdk.model.Item;
 
 import java.util.List;
+import java.util.Objects;
 
 public class KeyValue
 {
 	private String key;
 	private Object value;
 	private String variableType;
-	
+
 	public KeyValue()
 	{
 		setStringValue(null);
 	}
-	
+
 	public KeyValue(final KeyValue other)
 	{
 		this.key = other.key;
 		this.value = other.value;
 		this.variableType = other.variableType;
 	}
-	
+
 	public KeyValue(String key, String value)
 	{
 		this.key = key;
 		setStringValue(value);
 	}
-	
+
 	public void setKey(String key)
 	{
 		this.key = key;
 	}
-	
+
 	public String getKey()
 	{
 		return key;
 	}
-	
+
 	public void setStringValue(final String value)
 	{
 		this.value = value;
 		this.variableType = StandardPlaybookType.String.toString();
 	}
-	
+
 	public void setStringArrayValue(final List<String> value)
 	{
 		this.value = value;
 		this.variableType = StandardPlaybookType.StringArray.toString();
 	}
-	
+
 	public void setBinaryValue(final byte[] value)
 	{
 		this.value = value;
 		this.variableType = StandardPlaybookType.Binary.toString();
 	}
-	
+
 	public void setBinaryArrayValue(final byte[][] value)
 	{
 		this.value = value;
 		this.variableType = StandardPlaybookType.BinaryArray.toString();
 	}
-	
+
 	public void setTCEntityValue(final TCEntity value)
 	{
 		this.value = value;
 		this.variableType = StandardPlaybookType.TCEntity.toString();
 	}
-	
+
 	public void setTCEntityArrayValue(final List<TCEntity> value)
 	{
 		this.value = value;
 		this.variableType = StandardPlaybookType.TCEntityArray.toString();
 	}
-	
+
 	public void setTCEnhancedEntityValue(final Item value)
 	{
 		this.value = value;
 		this.variableType = StandardPlaybookType.TCEnhancedEntity.toString();
 	}
-	
+
 	public void setTCEnhancedEntityArrayValue(final List<Item> value)
 	{
 		this.value = value;
 		this.variableType = StandardPlaybookType.TCEnhancedEntityArray.toString();
 	}
-	
+
 	public void setCustomTypeValue(final byte[] value, final String type)
 	{
 		this.value = value;
 		this.variableType = type;
 	}
-	
+
 	public Object getValue()
 	{
 		return value;
 	}
-	
+
 	public String getVariableType()
 	{
 		return variableType;
 	}
-	
+
 	@Override
 	public String toString()
 	{
@@ -110,5 +111,22 @@ public class KeyValue
 			"key=" + key +
 			", value=" + value +
 			'}';
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		KeyValue keyValue = (KeyValue) o;
+		return Objects.equals(getKey(), keyValue.getKey()) &&
+				Objects.equals(getValue(), keyValue.getValue()) &&
+				Objects.equals(getVariableType(), keyValue.getVariableType());
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash(getKey(), getValue(), getVariableType());
 	}
 }
--- threatconnect-sdk/threatconnect-model/src/main/java/com/threatconnect/sdk/model/Item.java
+++ threatconnect-sdk/threatconnect-model/src/main/java/com/threatconnect/sdk/model/Item.java
@@ -1,6 +1,7 @@
 package com.threatconnect.sdk.model;
 
 import java.util.LinkedHashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public abstract class Item
@@ -9,38 +10,56 @@ public abstract class Item
 	private final Set<Attribute> attributes;
 	private final Set<String> tags;
 	private String xid;
-	
+
 	public Item(final ItemType itemType)
 	{
 		this.itemType = itemType;
 		this.attributes = new LinkedHashSet<Attribute>();
 		this.tags = new LinkedHashSet<String>();
 	}
-	
+
 	public final ItemType getItemType()
 	{
 		return itemType;
 	}
-	
+
 	public Set<Attribute> getAttributes()
 	{
 		return attributes;
 	}
-	
+
 	public Set<String> getTags()
 	{
 		return tags;
 	}
-	
+
 	public String getXid()
 	{
 		return xid;
 	}
-	
+
 	public void setXid(final String xid)
 	{
 		this.xid = xid;
 	}
-	
+
 	public abstract Set<? extends Item> getAssociatedItems();
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Item item = (Item) o;
+		return getItemType() == item.getItemType() &&
+				Objects.equals(getAttributes(), item.getAttributes()) &&
+				Objects.equals(getTags(), item.getTags()) &&
+				Objects.equals(getXid(), item.getXid());
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash(getItemType(), getAttributes(), getTags(), getXid());
+	}
 }
