569279a5eacbf40158a5d089ddb0de22b50adc91
Greg Marut <gmarut@threatconnect.com>
2018-06-22 11:33:08 -0400

    added new features

--- threatconnect-app/apps-util/src/main/java/com/threatconnect/app/addons/util/config/Feature.java
+++ threatconnect-app/apps-util/src/main/java/com/threatconnect/app/addons/util/config/Feature.java
@@ -9,4 +9,13 @@ public class Feature
 	public static final String SMTP_SETTINGS = "smtpSettings";
 	public static final String EMBEDDED_EXECUTION_ENABLED = "embeddedExecutionEnabled";
 	public static final String AOT_EXECUTION_ENABLED = "aotExecutionEnabled";
+	
+	public static final String CREATES_INDICATOR = "createsIndicator";
+	public static final String CREATES_GROUP = "createsGroup";
+	public static final String CREATES_TAG = "createsTag";
+	public static final String CREATES_SECURITY_LABEL = "createsSecurityLabel";
+	public static final String DELETES_INDICATOR = "deletesIndicator";
+	public static final String DELETES_GROUP = "deletesGroup";
+	public static final String DELETES_TAG = "deletesTag";
+	public static final String DELETES_SECURITY_LABEL = "deletesSecurityLabel";
 }
