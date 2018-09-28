c149d57ff0c6d7feef4ea6a7cbeb049c2bb80a50
Greg Marut <gmarut@threatconnect.com>
2018-09-28 10:05:53 -0400

    changed default packaged extension

--- threatconnect-sdk-addons/app-package-plugin/src/main/java/com/threatconnect/plugin/pkg/mojo/AbstractAppPackageMojo.java
+++ threatconnect-sdk-addons/app-package-plugin/src/main/java/com/threatconnect/plugin/pkg/mojo/AbstractAppPackageMojo.java
@@ -29,7 +29,7 @@ public abstract class AbstractAppPackageMojo extends AbstractPackageMojo<Install
 	
 	public static final Pattern PATTERN_INSTALL_JSON = Pattern.compile("^(?:(.*)\\.)?install\\.json$");
 	public static final String README_FILE_NAME = "README.md";
-	public static final String TC_APP_FILE_EXTENSION = "zip";
+	public static final String TC_APP_FILE_EXTENSION = "tcx";
 	public static final String TC_BUNDLED_FILE_EXTENSION = "bundle.zip";
 	
 	public AbstractAppPackageMojo()
