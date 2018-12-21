1e8cd15d3b700a2dbdd89cdc2e37076b6b5d68f3
Greg Marut <gmarut@threatconnect.com>
2018-12-21 13:49:12 -0500

    added output vars to layout

--- threatconnect-app/apps-util/src/main/java/com/threatconnect/app/addons/util/config/layout/validation/LayoutValidator.java
+++ threatconnect-app/apps-util/src/main/java/com/threatconnect/app/addons/util/config/layout/validation/LayoutValidator.java
@@ -3,6 +3,7 @@ package com.threatconnect.app.addons.util.config.layout.validation;
 import com.threatconnect.app.addons.util.config.install.Install;
 import com.threatconnect.app.addons.util.config.layout.Layout;
 import com.threatconnect.app.addons.util.config.layout.LayoutInput;
+import com.threatconnect.app.addons.util.config.layout.LayoutOutput;
 import com.threatconnect.app.addons.util.config.validation.ValidationException;
 import com.threatconnect.app.addons.util.config.validation.Validator;
 
@@ -18,11 +19,18 @@ public class LayoutValidator extends Validator<Layout>
 	@Override
 	public void validate(final Layout object) throws ValidationException
 	{
-		//for each of the groups
+		//for each of the inputs
 		LayoutInputValidator layoutInputValidator = new LayoutInputValidator(install);
-		for(LayoutInput layoutInput : object.getInputs())
+		for (LayoutInput layoutInput : object.getInputs())
 		{
 			layoutInputValidator.validate(layoutInput);
+		}
+		
+		//for each of the outputs
+		LayoutOutputValidator layoutOutputValidator = new LayoutOutputValidator(install);
+		for (LayoutOutput layoutOutput : object.getOutputs())
+		{
+			layoutOutputValidator.validate(layoutOutput);
 		}
 	}
 }
