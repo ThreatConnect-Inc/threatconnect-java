84ff165f4511b461b82d5f23a17efb39dfa31120
Greg Marut <gmarut@threatconnect.com>
2019-06-20 14:03:12 -0400

    validating api service classes on package

--- threatconnect-sdk-addons/app-package-plugin/src/main/java/com/threatconnect/plugin/pkg/mojo/JavaPackageMojo.java
+++ threatconnect-sdk-addons/app-package-plugin/src/main/java/com/threatconnect/plugin/pkg/mojo/JavaPackageMojo.java
@@ -1,28 +1,32 @@
 package com.threatconnect.plugin.pkg.mojo;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonSerializer;
 import com.threatconnect.app.addons.util.config.Feature;
 import com.threatconnect.app.addons.util.config.install.Install;
 import com.threatconnect.app.addons.util.config.install.InstallUtil;
+import com.threatconnect.app.addons.util.config.install.RunLevelType;
 import com.threatconnect.app.addons.util.config.validation.InstallValidator;
 import com.threatconnect.app.addons.util.config.validation.ValidationException;
+import com.threatconnect.app.apps.service.api.ApiService;
+import com.threatconnect.app.apps.service.api.mapping.ApiMapper;
+import com.threatconnect.app.apps.service.api.mapping.ApiMapping;
 import com.threatconnect.plugin.pkg.JavaPackageFileFilter;
 import com.threatconnect.plugin.pkg.PackageFileFilter;
 import com.threatconnect.plugin.pkg.Profile;
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Collection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.List;
 
 @Mojo(name = "java-package", defaultPhase = LifecyclePhase.PACKAGE, threadSafe = true, requiresDependencyResolution = ResolutionScope.RUNTIME)
 public class JavaPackageMojo extends AbstractAppPackageMojo
@@ -35,6 +39,9 @@ public class JavaPackageMojo extends AbstractAppPackageMojo
 	 */
 	@Parameter
 	private String classifier;
+	
+	@Parameter(defaultValue = "${project}", readonly = true, required = true)
+	private MavenProject project;
 	
 	public JavaPackageMojo()
 	{
@@ -100,9 +107,60 @@ public class JavaPackageMojo extends AbstractAppPackageMojo
 					}
 					
 					super.validate(object);
+					
+					if (RunLevelType.ApiService == object.getRuntimeLevel())
+					{
+						loadAndValidateApiServiceClass(object.getMainAppClass());
+					}
 				}
 			}
 		);
+	}
+	
+	private void loadAndValidateApiServiceClass(final String mainAppClass) throws ValidationException
+	{
+		//validate the program language
+		if (null == mainAppClass)
+		{
+			throw new ValidationException("mainAppClass is not defined.");
+		}
+		
+		try
+		{
+			List<String> runtimeClasspathElements = project.getRuntimeClasspathElements();
+			URL[] runtimeUrls = new URL[runtimeClasspathElements.size()];
+			for (int i = 0; i < runtimeClasspathElements.size(); i++)
+			{
+				String element = runtimeClasspathElements.get(i);
+				runtimeUrls[i] = new File(element).toURI().toURL();
+			}
+			URLClassLoader newLoader = new URLClassLoader(runtimeUrls, Thread.currentThread().getContextClassLoader());
+			
+			//retrieve the main app class
+			final Class<? extends ApiService> apiServiceClass = (Class<? extends ApiService>) newLoader.loadClass(mainAppClass);
+			
+			if (!ApiService.class.isAssignableFrom(apiServiceClass))
+			{
+				throw new ValidationException(mainAppClass + " must extend " + ApiService.class.getName());
+			}
+			
+			//retrieve all of the service items from the main class
+			ApiMapper apiMapper = new ApiMapper(apiServiceClass);
+			if (apiMapper.getServiceItems().isEmpty())
+			{
+				throw new ValidationException(
+					mainAppClass + " does not define any api endpoints. Please add at least one @" + ApiMapping.class.getName()
+						+ " annotated method.");
+			}
+		}
+		catch (ClassNotFoundException e)
+		{
+			throw new ValidationException("Unable to load class: " + mainAppClass);
+		}
+		catch (ClassCastException | MalformedURLException | DependencyResolutionRequiredException e)
+		{
+			throw new RuntimeException(e);
+		}
 	}
 	
 	protected File getSourceJarFile()
