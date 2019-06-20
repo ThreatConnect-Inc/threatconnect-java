996ccbabb6d8c8d81172302a6b070f906c4dac8f
Greg Marut <gmarut@threatconnect.com>
2019-06-20 11:47:51 -0400

    fixed import

--- threatconnect-app/apps-util/src/main/java/com/threatconnect/app/addons/util/config/validation/InstallValidator.java
+++ threatconnect-app/apps-util/src/main/java/com/threatconnect/app/addons/util/config/validation/InstallValidator.java
@@ -11,7 +11,6 @@ import com.threatconnect.app.addons.util.config.install.RunLevelType;
 import com.threatconnect.app.addons.util.config.install.ServerVersion;
 import com.threatconnect.app.addons.util.config.layout.Layout;
 import com.threatconnect.app.addons.util.config.layout.validation.LayoutValidator;
-import com.threatconnect.app.addons.util.config.validation.service.ApiServiceValidator;
 
 import java.util.HashSet;
 import java.util.Set;
