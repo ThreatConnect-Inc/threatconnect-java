836d004fadff8d72ebd6440571a6d93878f1e362
Greg Marut <gmarut@threatconnect.com>
2019-07-18 09:54:08 -0400

    added logging change message

--- threatconnect-app/services-core/src/main/java/com/threatconnect/app/services/message/LoggingChange.java
+++ threatconnect-app/services-core/src/main/java/com/threatconnect/app/services/message/LoggingChange.java
@@ -2,8 +2,20 @@ package com.threatconnect.app.services.message;
 
 public class LoggingChange extends CommandMessage
 {
+	private String level;
+	
 	public LoggingChange()
 	{
 		super(CommandType.LoggingChange);
+	}
+	
+	public String getLevel()
+	{
+		return level;
+	}
+	
+	public void setLevel(final String level)
+	{
+		this.level = level;
 	}
 }
