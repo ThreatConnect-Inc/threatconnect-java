66ce83d3283696cc8ebb6082e09eeb867f26dc12
Greg Marut <gmarut@threatconnect.com>
2020-08-06 12:25:19 -0400

    added a new discoveryTypes string list to ready command

--- threatconnect-app/services-core/src/main/java/com/threatconnect/app/services/message/Ready.java
+++ threatconnect-app/services-core/src/main/java/com/threatconnect/app/services/message/Ready.java
@@ -1,9 +1,23 @@
 package com.threatconnect.app.services.message;
 
+import java.util.List;
+
 public class Ready extends AbstractCommandConfig
 {
+	private List<String> discoveryTypes;
+	
 	public Ready()
 	{
 		super(CommandType.Ready);
+	}
+	
+	public List<String> getDiscoveryTypes()
+	{
+		return discoveryTypes;
+	}
+	
+	public void setDiscoveryTypes(final List<String> discoveryTypes)
+	{
+		this.discoveryTypes = discoveryTypes;
 	}
 }
