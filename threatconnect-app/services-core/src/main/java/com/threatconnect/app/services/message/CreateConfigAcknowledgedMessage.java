f6bdc4860dbe652f9874a732b5ae0c15471d8ed4
Greg Marut <gmarut@threatconnect.com>
2020-08-27 15:32:40 -0400

    added log file to create config ack message

--- threatconnect-app/services-core/src/main/java/com/threatconnect/app/services/message/CreateConfigAcknowledgedMessage.java
+++ threatconnect-app/services-core/src/main/java/com/threatconnect/app/services/message/CreateConfigAcknowledgedMessage.java
@@ -2,8 +2,20 @@ package com.threatconnect.app.services.message;
 
 public class CreateConfigAcknowledgedMessage extends AcknowledgedMessage
 {
+	private String logFile;
+	
 	public CreateConfigAcknowledgedMessage()
 	{
 		super(CommandType.CreateConfig);
+	}
+	
+	public String getLogFile()
+	{
+		return logFile;
+	}
+	
+	public void setLogFile(final String logFile)
+	{
+		this.logFile = logFile;
 	}
 }
