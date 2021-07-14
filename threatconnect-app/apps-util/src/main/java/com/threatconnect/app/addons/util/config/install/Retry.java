8cf95aeefe9a031111574edceb48096d95a04685
Greg Marut <gmarut@threatconnect.com>
2021-07-14 14:07:55 -0400

    added new config field for tc_action

--- threatconnect-app/apps-util/src/main/java/com/threatconnect/app/addons/util/config/install/Retry.java
+++ threatconnect-app/apps-util/src/main/java/com/threatconnect/app/addons/util/config/install/Retry.java
@@ -1,11 +1,14 @@
 package com.threatconnect.app.addons.util.config.install;
 
+import java.util.List;
+
 /**
  * @author Greg Marut
  */
 public class Retry
 {
 	private boolean allowed;
+	private List<String> actions;
 	private Integer defaultDelayMinutes;
 	private Integer defaultMaxRetries;
 	
@@ -17,6 +20,16 @@ public class Retry
 	public void setAllowed(final boolean allowed)
 	{
 		this.allowed = allowed;
+	}
+	
+	public List<String> getActions()
+	{
+		return actions;
+	}
+	
+	public void setActions(final List<String> actions)
+	{
+		this.actions = actions;
 	}
 	
 	public Integer getDefaultDelayMinutes()
