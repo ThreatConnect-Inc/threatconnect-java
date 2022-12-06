8fc4bdf0df17c1d81ff96c1794ca2a2e8749f069
Greg Marut <gmarut@threatconnect.com>
2022-12-06 09:42:29 -0500

    added new feature flag

--- threatconnect-app/apps-util/src/main/java/com/threatconnect/app/addons/util/config/Feature.java
+++ threatconnect-app/apps-util/src/main/java/com/threatconnect/app/addons/util/config/Feature.java
@@ -18,6 +18,7 @@ public class Feature
 	public static final String WEBHOOK_DEACTIVATE_RESPONSE = "webhookDeactivateResponse";
 	public static final String RUNTIME_VARIABLES = "runtimeVariables";
 	public static final String INSTANCE_UUID = "instanceUUID";
+	public static final String LINK_API_PATH = "linkApiPath";
 	
 	public static final String CREATES_INDICATOR = "CreatesIndicator";
 	public static final String CREATES_GROUP = "CreatesGroup";
