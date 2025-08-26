ad3b13e4d5bc5d3c740c35abe21049e080c3867b
Greg Marut <gmarut@threatconnect.com>
2025-08-26 10:54:10 -0400

    added new runlevel for PlaybookAgent

--- threatconnect-app/apps-util/src/main/java/com/threatconnect/app/addons/util/config/install/RunLevelType.java
+++ threatconnect-app/apps-util/src/main/java/com/threatconnect/app/addons/util/config/install/RunLevelType.java
@@ -5,6 +5,6 @@ package com.threatconnect.app.addons.util.config.install;
  */
 public enum RunLevelType
 {
-	System, Organization, SpaceOrganization, SpaceSystem, Internal, StixParser, Playbook, ThirdParty, PlaybookOperator,
+	System, Organization, SpaceOrganization, SpaceSystem, Internal, StixParser, Playbook, PlaybookAgent, ThirdParty, PlaybookOperator,
 	TriggerService, ApiService, FeedApiService, WebHookTriggerService, AdminService
 }
